e51d01a89d6475425d6e60d4f446af1ef5d7d08d
ivoangelovski <ivo.angelovski@it-labs.com>
2015-07-24 16:21:22 +0200

    t srv

---
+++ tornado_server.py
@@ -0,0 +1,13 @@
+import tornado.web
+import tornado.ioloop
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
+
+application.listen(8888)
+tornado.ioloop.IOLoop.instance().start()
